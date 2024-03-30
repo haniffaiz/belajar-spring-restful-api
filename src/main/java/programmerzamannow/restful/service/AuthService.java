d0cab760b66c1f3644d4ed7cb111f1625cf37cf6
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-30 21:13:57 +0700

    logout user api

--- src/main/java/programmerzamannow/restful/service/AuthService.java
+++ src/main/java/programmerzamannow/restful/service/AuthService.java
@@ -47,4 +47,11 @@ public class AuthService {
         return System.currentTimeMillis()+(1000 *16*24*30);
     }
 
+    @Transactional
+    public void logout(User user){
+        user.setToken(null);
+        user.setTokenExpiredAt(null);
+
+        userRepository.save(user);
+    }
 }
